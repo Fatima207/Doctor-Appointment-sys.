36a3f107a7a8e4d6c129dad26ec4f7e5f04f5c4a
mahvishfatima162@gmail.com <mahvishfatima162@gmail.com>
2024-07-06 18:40:23 +0530

    connected frontend with backend using concurrently

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
+import "../styles/RegisterStyles.css";
+import { Form, Input, message } from "antd";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
-const login = () => {
+const Login = () => {
+  
+  //form handler
+  const onfinishHandler =(values) => {
+    console.log(values)
+  };
   return (
-    <div>
-      login
-    </div>
-  )
-}
+    <div className="form-container ">
+      <Form
+        layout="vertical"
+        onFinish={onfinishHandler}
+        className="register-form"
+      >
+        <h3 className="text-center">Login From</h3>
 
-export default login
+        <Form.Item label="Email" name="email">
+          <Input type="email" required />
+        </Form.Item>
+        <Form.Item label="Password" name="password">
+          <Input type="password" required />
+        </Form.Item>
+        <Link to="/register" className="m-2">
+          Not a user Register here
+        </Link>
+        <button className="btn btn-primary" type="submit">
+          Login
+        </button>
+      </Form>
+    </div>
+  );
+};
+
+export default Login;
--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -3,7 +3,37 @@ const userModel=require('../models/userModels')
 const bcrypt=require('bcryptjs')
 
 //register callback
-const registerController=()=>{}
+const registerController= async (req,res)=>{
+    try {
+        
+        const existingUser=await userModel.findOne({email:req.body.email})
+
+        //check if user is already registered or not by matching his email
+
+        if(existingUser){
+            return res.status(200).send({message:'User Already Exists',success:false})
+        }
+
+        //encrypt the password
+
+        const password=req.body.password
+        const salt=await bcrypt.genSalt(10)
+        const hashedPassword=await bcrypt.hash(password,salt)
+        
+        // salting done, now replace encrypted password by original password i.e. without encrypted.
+        request.body.password=hashedPassword
+
+        //creating new user using user model
+        const newUser= new userModel(req.body);
+        await newUser.save();
+        res.status(201).send({message: "Registration successful",success:true})
+
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({success:false,message:`Register Controller ${error.message}`})
+    }
+}
 
 const loginController=()=>{}
 
